c92618124827a8e52295a50e6ed54569330651ec
TurtlePower <agnkvdm@gmail.com>
2023-10-19 21:48:05 +0300

    add register and login controllers

--- backend/controllers/users/login.js
+++ backend/controllers/users/login.js
@@ -1,41 +1,39 @@
-const { Unauthorized } = require('http-errors');
-const { User } = require('../../models/user');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { SECRET_KEY } = process.env;
+// const { Unauthorized } = require('http-errors');
+// const { User } = require('../../models/user');
+// const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+// const { SECRET_KEY } = process.env;
 
 const login = async (req, res, next) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  // const user = await User.findOne({ email });
 
-  if (!user) {
-    throw new Unauthorized(`Email or password is wrong`);
-  }
+  // if (!user) {
+  //   throw new Unauthorized(`Email or password is wrong`);
+  // }
 
-  const passCompare = bcrypt.compareSync(password, user.password);
+  // const passCompare = bcrypt.compareSync(password, user.password);
 
-  if (!passCompare) {
-    throw new Unauthorized('Email or password is wrong');
-  }
+  // if (!passCompare) {
+  //   throw new Unauthorized('Email or password is wrong');
+  // }
 
-  if (!user.verify) {
-    throw new Unauthorized("Email isn't verified");
-  }
+  // const payload = {
+  //   id: user._id,
+  // };
 
-  const payload = {
-    id: user._id,
-  };
-
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
-  await User.findByIdAndUpdate(user._id, { token });
+  // const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+  // await User.findByIdAndUpdate(user._id, { token });
 
   res.json({
     status: 'success',
     code: 200,
     data: {
-      token,
+      // token,
       user: {
-        email: user.email,
+        // email: user.email,
+        email,
+        password,
       },
     },
   });
--- backend/controllers/users/register.js
+++ backend/controllers/users/register.js
@@ -1,22 +1,27 @@
-const { Conflict } = require('http-errors');
-const { User } = require('../../models/user');
+// const { Conflict } = require('http-errors');
+// const { User } = require('../../models/user');
 const bcrypt = require('bcryptjs');
 
 const register = async (req, res, next) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  // const user = await User.findOne({ email });
 
-  if (user) {
-    throw new Conflict(`User with email ${email} already exist`);
-  }
+  // if (user) {
+  //   throw new Conflict(`User with email ${email} already exist`);
+  // }
 
   const salt = bcrypt.genSaltSync(10);
   const hashPassword = bcrypt.hashSync(password, salt);
 
-  const { email: userEmail } = await User.create({
+  const { email: userEmail } = {
     email,
     password: hashPassword,
-  });
+  };
+
+  // const { email: userEmail } = await User.create({
+  //   email,
+  //   password: hashPassword,
+  // });
 
   res.status(201).json({
     status: 'success',
