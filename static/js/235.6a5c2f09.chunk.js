473b30aef24482f771f4b51bb80d47fad55e2b94
made-by-curiosity <93835953+made-by-curiosity@users.noreply.github.com>
2023-10-19 12:55:06 +0000

    Deploying to gh-pages from @ made-by-curiosity/zenbit_test_task@f1088b64fcdcd6555f553413998ddd006b702e30 🚀

---
+++ static/js/235.6a5c2f09.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkzenbit_test_task=self.webpackChunkzenbit_test_task||[]).push([[235],{4235:function(t,e,n){n.r(e);var s=n(5834),u=n(3329);e.default=function(){return(0,u.jsx)(s.W,{children:"SIGN UP PAGE"})}}}]);
+//# sourceMappingURL=235.6a5c2f09.chunk.js.map
