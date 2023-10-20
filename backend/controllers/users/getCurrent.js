692e9b6df3c491a54816f9a2d486a3b367a6a160
TurtlePower <agnkvdm@gmail.com>
2023-10-20 17:10:53 +0300

    add get current user info controller

--- backend/controllers/users/getCurrent.js
+++ backend/controllers/users/getCurrent.js
@@ -2,12 +2,8 @@ const getCurrent = async (req, res, next) => {
   const { email } = req.user;
 
   res.json({
-    status: 'success',
-    code: 200,
-    data: {
-      user: {
-        email,
-      },
+    user: {
+      email,
     },
   });
 };
